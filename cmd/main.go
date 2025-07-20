50c14762b5f33306f940981aa524c691f09a00cf
Anatolii Kovalenko <kab1993@uandex.ru>
2025-07-20 17:26:12 +0300

    update

--- cmd/main.go
+++ cmd/main.go
@@ -14,6 +14,15 @@ func main() {
 	logger.Printf("Starting web-server at %s", srv.Server.Addr)
 	logger.Println(os.Getwd())
 
+	entries, err := os.ReadDir("../")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range entries {
+		log.Println(e.Name())
+	}
+
 	if err := srv.Server.ListenAndServe(); err != nil {
 		log.Fatalf("error while starting the server: %s", err.Error())
 		return
