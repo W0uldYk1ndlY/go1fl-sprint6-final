05d1af36297cd2603b21a3cc78943fe319a4484c
Anatolii Kovalenko <kab1993@uandex.ru>
2025-07-20 16:19:39 +0300

    fix filepath

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,8 @@ func main() {
 	logger := log.New(os.Stdout, "LOG:", 0)
 	srv := server.NewServer(logger)
 
+	logger.Printf("Starting web-server at %s", srv.Server.Addr)
+
 	if err := srv.Server.ListenAndServe(); err != nil {
 		log.Fatalf("error while starting the server: %s", err.Error())
 		return
